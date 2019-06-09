9a62d6b18e78407a4f0854bc50ccd1bf27908549
Matthew McCord <mattm.eternal@gmail.com>
2019-06-08 17:23:27 -0700

    Create entity_manager module

---
+++ modules/entities/entity_manager.lua
@@ -0,0 +1,15 @@
+EntityManager = Class{
+    init = function(self, handler)
+        self.handler = handler
+        -- self.player = player
+        -- self.entities = new Array(player)
+    end;
+
+    update = function(self, deltaTime)
+        --
+    end;
+
+    draw = function(self)
+        --
+    end;
+}
