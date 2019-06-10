5719914f9bd2b471a20a54f093645254db0790ae
Matthew McCord <mattm.eternal@gmail.com>
2019-06-09 20:32:14 -0700

    Create entity module

---
+++ modules/entities/entity.lua
@@ -0,0 +1,9 @@
+Entity = Class {
+    init = function(self, handler, x, y)
+        print("HELLLOOO")
+        self.handler = handler;
+        self.x = x;
+        self.y = y;
+        -- self.b = new Rectangle(0, 0, TILE_SIZE, TILE_SIZE);
+    end
+}
