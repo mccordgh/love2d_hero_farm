bfb3bb86259cd7929352c2b0efc630349ceefd16
Matthew McCord <mattm.eternal@gmail.com>
2019-06-09 21:39:46 -0700

    Swap hardcoded numbers for GameConstants values

--- modules/worlds/world_one.lua
+++ modules/worlds/world_one.lua
@@ -1,4 +1,3 @@
-
 WorldOne = Class{
     init = function(self, handler, entityManager)
         self.handler = handler
@@ -19,11 +18,45 @@ WorldOne = Class{
         -- draw background
         self:drawTiles()
 
+        -- draw horizontal borders
+        self:drawHorizontalBorders()
+
+        -- draw vertical borders
+        self:drawVerticalBorders()
+
         -- draw entities
         self.entityManager:draw()
     end;
 
+    drawHorizontalBorders = function(self)
+        love.graphics.setColor(0.6, 0.6, 0.6, 1)
+
+        for y = 0, GameConstants.verticalTileCount - 1, 1
+        do
+            love.graphics.rectangle("fill", 0, y * GameConstants.tileWidth, GameConstants.tileSize, GameConstants.tileSize)
+            love.graphics.rectangle("fill", (GameConstants.horizontalTileCount - 1) * GameConstants.tileWidth, y * GameConstants.tileHeight, GameConstants.tileSize, GameConstants.tileSize)
+        end
+    end;
+
+    drawVerticalBorders = function(self)
+        love.graphics.setColor(0.3, 0.3, 0.3, 1)
+
+        for x = 1, GameConstants.horizontalTileCount - 1, 1
+        do
+            love.graphics.rectangle("fill", x * GameConstants.tileWidth, 0, GameConstants.tileSize, GameConstants.tileSize)
+            love.graphics.rectangle("fill", x * GameConstants.tileWidth, (GameConstants.verticalTileCount - 1) * GameConstants.tileHeight, GameConstants.tileSize, GameConstants.tileSize)
+        end
+    end;
+
     drawTiles = function(self)
-        --
+        love.graphics.setColor(1, 1, 1, 1)
+
+        for y = 1, GameConstants.verticalTileCount - 2, 1
+        do
+            for x = 1, GameConstants.horizontalTileCount - 2, 1
+            do
+                love.graphics.rectangle("line", x * GameConstants.tileWidth, y * GameConstants.tileHeight, GameConstants.tileSize, GameConstants.tileSize)
+            end
+        end
     end
 }
