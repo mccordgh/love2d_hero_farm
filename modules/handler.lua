127d7311ef613497d0625e8782400c5e0558e64e
Matthew McCord <mattm.eternal@gmail.com>
2019-06-22 17:05:33 -0700

    Add AssetManager and TileManager to handler

--- modules/handler.lua
+++ modules/handler.lua
@@ -1,11 +1,18 @@
+require('modules.graphics.asset_manager')
 require('modules.states.state_manager')
+require('modules.tiles.tile_manager')
 require('modules.worlds.world_manager')
+-- require('debug.debugger')
 
 handler = Class{
     init = function(self, game)
         self.game = game
 
+        -- self.debugger = Debugger(self)
+
+        self.assetManager = AssetManager(self)
         self.stateManager = StateManager(self)
+        self.tileManager = TileManager(self)
         self.worldManager = WorldManager(self)
     end;
 
@@ -22,11 +29,24 @@ handler = Class{
         -- self.mouseManager.mouseReleased(x, y, button, isTouch)
     end;
 
+    -- getDebugger = function(self)
+    --     return self.debugger
+    -- end;
+
+    getAssetManager = function(self)
+        return self.assetManager
+    end;
+
     getStateManager = function(self)
         return self.stateManager
+    end;
+
+    getTileManager = function(self)
+        return self.tileManager
     end;
 
     getWorldManager = function(self)
         return self.worldManager
     end;
 }
+
