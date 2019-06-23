48afac0ad9f6b0a37771c882a61f8a3973fdeb6f
Matthew McCord <mattm.eternal@gmail.com>
2019-06-22 17:00:58 -0700

    Create debugger helper class

---
+++ debug/debugger.lua
@@ -0,0 +1,15 @@
+-- Debugger = Class{
+--     init = function(self)
+
+--     end;
+
+--     log = function(text)
+--         ffile = io.open("debug/debug.log", "a")
+
+--         io.output(ffile)
+
+--         io.write("\n" .. text)
+
+--         io.close(ffile)
+--     end;
+-- }
