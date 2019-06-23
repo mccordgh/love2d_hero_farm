4161ab0148ef119af49abd7fb49f218cec70d90c
Matthew McCord <mattm.eternal@gmail.com>
2019-06-22 17:03:34 -0700

    Set up game constants for 1 or 2x scale

--- constants/game_constants.lua
+++ constants/game_constants.lua
@@ -1,10 +1,23 @@
 GameConstants = {
-    TileHeight = 32,
-    TileWidth = 32,
+    TileHeight = 32 * 2,
+    TileWidth = 32 * 2,
+    TileScale = 2,
 
     HorizontalTileCount = 10,
     VerticalTileCount = 16,
 
-    PlayerWidth = 32,
-    PlayerHeight = 32,
+    PlayerWidth = 32 * 2,
+    PlayerHeight = 48 * 2,
 }
+
+-- GameConstants = {
+--     TileHeight = 32,
+--     TileWidth = 32,
+    -- TileScale = 1
+
+--     HorizontalTileCount = 10,
+--     VerticalTileCount = 16,
+
+--     PlayerWidth = 32,
+--     PlayerHeight = 48,
+-- }
