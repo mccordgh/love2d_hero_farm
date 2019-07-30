df859a1a2373cb77a8038677b047a28acf63319b
Matthew McCord <mattm.eternal@gmail.com>
2019-07-30 16:20:29 -0700

    Add WASD to key bindings

--- modules/input/key_manager.lua
+++ modules/input/key_manager.lua
@@ -9,10 +9,16 @@ KeyManager = Class {
 
     initKeys = function(self)
         self.keys["escape"] = false
+
         self.keys["up"] = false
         self.keys["down"] = false
         self.keys["left"] = false
         self.keys["right"] = false
+
+        self.keys["w"] = false
+        self.keys["s"] = false
+        self.keys["a"] = false
+        self.keys["d"] = false
     end;
 
     keyPressed = function(self, key, scan_code, is_repeat)
@@ -24,19 +30,19 @@ KeyManager = Class {
     end;
 
     getUpKey = function(self)
-        return self.keys["up"]
+        return self.keys["up"] or self.keys["w"]
     end;
 
     getDownKey = function(self)
-        return self.keys["down"]
+        return self.keys["down"] or self.keys["s"]
     end;
 
     getLeftKey = function(self)
-        return self.keys["left"]
+        return self.keys["left"] or self.keys["a"]
     end;
 
     getRightKey = function(self)
-        return self.keys["right"]
+        return self.keys["right"] or self.keys["d"]
     end;
 
     getEscapeKey = function(self)
